5fc02e858ada346a8a96b751edfc7e1ee9909cc9
josh28xd <joshvalencia114@gmail.com>
2022-12-01 00:54:09 +0800

    StoreOwnerActivity

---
+++ Finals - Valencia, Yu/app/src/main/java/com/example/code12_firebaseauthentication/activities/StoreOwnerActivity.java
@@ -0,0 +1,16 @@
+package com.example.code12_firebaseauthentication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.code12_firebaseauthentication.R;
+
+public class StoreOwnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_owner);
+    }
+}
